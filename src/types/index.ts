efeb1aeff0f56fdd6be4b3cc9dc5a4393eacaabf
Sweta Shaw <swt.shw94@gmail.com>
2023-05-04 08:39:20 +0530

    remove deprecated github providers

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,60 +1,40 @@
 import { BigNumber } from 'ethers'
-import { GithubCommits, GithubPullRequests, GithubRepository, GithubRepositoryLang, GithubRepositoryTopics, KeyOf, QueryStringObject } from './utils'
 
 export type ProviderParams =
 	| {
 		provider: 'google-login'
-		parameters: {}
+		payload: {}
 	}
 	| {
 		provider: 'yc-login'
-		parameters: {}
+		payload: {}
 	}
 	| {
 		provider: 'github-claim'
-		parameters: GithubParams
-	}
-	| {
-		provider: 'github-contributed'
-		parameters: GithubLoginParams<'github-contributed'>
-	}
-	| {
-		provider: 'github-commits'
-		parameters: GithubLoginParams<'github-commits'>
-	}
-	| {
-		provider: 'github-issues'
-		parameters: GithubLoginParams<'github-issues'>
-	}
-	| {
-		provider: 'github-languages'
-		parameters: GithubLoginParams<'github-languages'>
-	}
-	| {
-		provider: 'github-pullRequests'
-		parameters: GithubLoginParams<'github-pullRequests'>
+		payload: GithubParams
 	}
 	| {
 		provider: 'http'
-		metadata: {
-			name: string
-			logoUrl: string
-		}
-		method: 'GET' | 'POST'
-		url: string
-		login: {
+		payload: {
+			metadata: {
+				name: string
+				logoUrl: string
+			}
+			method: 'GET' | 'POST'
 			url: string
-			checkLoginCookies: string[]
-		}
-		responseSelections: responseSelection[]
-		parameters: {
-				[key: string]: string
+			login: {
+				url: string
+				checkLoginCookies: string[]
+			}
+			responseSelections: responseSelection[]
+			parameters: {
+					[key: string]: string
+			}
 		}
 	}
 
 
 export type responseSelection = { responseMatch: string }
-export type ProviderName = ProviderParams['provider']
 
 export type Claim = {
 	templateClaimId: string
@@ -82,36 +62,6 @@ export type RequestClaim = {
 	owner: string
 	timestampS: number
 	claimId: BigNumber
-}
-
-export type ApiType = keyof GithubApiProvider
-
-export type GithubApi = {
-	'github-topics': GithubRepositoryTopics
-	'github-contributed': GithubRepository[]
-	'github-languages': GithubRepositoryLang
-	'github-commits': GithubCommits[]
-	'github-pullRequests': GithubPullRequests[]
-	'github-issues': GithubPullRequests[]
-}
-
-export type PartialObj<T> = {
-	readonly [K in keyof T]: T[K] extends Array<infer U> ? Partial<U>[] : T[K]
-}
-
-export type GithubApiProvider = PartialObj<GithubApi>
-
-export type GithubLoginParams<T extends ApiType> = {
-	/** the github repo in the format `owner/repo` */
-	repo: string
-	/** query string for the path eg: per_page: 100 */
-	qs?: QueryStringObject
-	/** type of the provider eg: `github-commits` */
-	type: T
-	/** the response object to match with the provider */
-	response?: GithubApiProvider[T]
-	/** the array of keys which user wants to verify eg: ['sha', 'node_id'] in case of github-commits `type` */
-	keys: KeyOf<GithubApiProvider[T]>[]
 }
 
 type GithubClaimType = 'issues' | 'commits' | 'repositories'
