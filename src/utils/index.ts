ed98c593d29b1d65dc4e9a75ca363316edc18dd8
Sweta Shaw <swt.shw94@gmail.com>
2023-05-18 18:21:56 +0530

    add function to extract values from http proofs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers'
 import { v4 as uuidv4 } from 'uuid'
-import { Proof, RequestClaim } from '../types'
+import { Proof, RequestClaim, responseSelection } from '../types'
 import CONTRACTS_CONFIG from '../utils/contracts/config.json'
 import { Reclaim, Reclaim__factory as ReclaimFactory } from '../utils/contracts/types'
 
@@ -56,4 +56,47 @@ export function getContract(chainId: number) {
 export function getProofsFromRequestBody(requestBody: string) {
 	const proofs: Proof[] = JSON.parse(decodeURIComponent(requestBody)).proofs
 	return proofs
+}
+
+export function extractValuesFromParameters(responseSelections: responseSelection[], proof: Proof) {
+	// check if correct number of response selections are present
+	if(proof.parameters.responseSelections && (responseSelections.length === proof.parameters.responseSelections.length)) {
+
+		// create object to store parameter values
+		const parameterObj: {[key: string]: string} = {}
+
+		// iterate over all response selections
+		for(let i = 0; i < responseSelections.length; i++) {
+			const proofResponseSelection = proof.parameters.responseSelections[i] as responseSelection
+
+			if(proofResponseSelection.responseMatch) {
+				// get regex string from response selection
+				const responseMatchRegex = responseSelections[i].responseMatch
+
+				const parameterKeys: string[] = []
+				// replace all {{parameterName}} with (.*?)
+				const regexString = responseMatchRegex.replace(/{{(.*?)}}/g, (_, parameterName) => {
+					parameterKeys.push(parameterName)
+					return '(.*?)'
+				})
+
+				// create regex from string
+				const regex = new RegExp(regexString, 'g')
+
+				const regexStringWithValues = proofResponseSelection.responseMatch
+				const regexValues = regex.exec(regexStringWithValues)
+				if(regexValues !== null) {
+					for(let i = 0; i < parameterKeys.length; i++) {
+						const parameterKey = parameterKeys[i]
+						const parameterValue = regexValues[i + 1]
+						parameterObj[parameterKey] = parameterValue
+					}
+				}
+			}
+		}
+
+		return parameterObj
+	} else {
+		throw new Error('Invalid response selections')
+	}
 }
