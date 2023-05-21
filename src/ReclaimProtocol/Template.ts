a15db1cdb00f2eff7950f6dd4b4a077e7b917196
Sweta Shaw <swt.shw94@gmail.com>
2023-05-22 00:11:38 +0530

    rename reclaimUrl to getReclaimUrl

--- src/ReclaimProtocol/Template.ts
+++ src/ReclaimProtocol/Template.ts
@@ -47,7 +47,7 @@ export default class TemplateInstance {
      * Getter template url
      * @return {string}
     */
-	get reclaimUrl(): string {
+	get getReclaimUrl(): string {
 		return RECLAIM_APP_URL + encodeURIComponent(JSON.stringify(this._template))
 	}
 
