e2bc46c367d9b0bf46697f53bff43d9b10e8ab31
Sweta Shaw <swt.shw94@gmail.com>
2023-05-23 21:42:09 +0530

    refactor frontend code

--- examples/example-frontend/src/App.tsx
+++ examples/example-frontend/src/App.tsx
@@ -2,24 +2,16 @@ import React from 'react';
 import './App.css';
 import axios from 'axios'
 import QRCode from 'react-qr-code';
-
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const backendBaseUrl = process.env.REACT_APP_BACKEND_BASE_URL
-const getStatusUrl = process.env.REACT_APP_BACKEND_BASE_URL + '/status'
 
 function App() {
 
-  const [callbackUrl, setCallbackUrl] = React.useState<string | null>(null)
   const [callbackId, setCallbackId] = React.useState<string | null>(null)
-  const [status, setStatus] = React.useState<string | null>(null)
-  const [loading, setLoading] = React.useState<boolean>(false)
-  const [username, setUsername] = React.useState<string>()
-  const [appUrl, setAppUrl] = React.useState<string | null>(null)
+  const [reclaimUrl, setReclaimUrl] = React.useState<string | null>(null)
 
-  const getStatus = async (callbackId: string) => {
-    const response = await axios.get(getStatusUrl + `/${callbackId}`);
-    setStatus(response.data.status);
-  }
+  const notify = () => alert('Link copied to clipboard!');
 
   const proveIt = async (e: any) => {
     e.preventDefault();
@@ -27,70 +19,57 @@ function App() {
     const response = await axios.get(backendBaseUrl + '/request')
     console.log(response);
     setCallbackId(response.data.callbackId)
-    setCallbackUrl(response.data.reclaimUrl);
-    setLoading(true)
     //window.location.replace(response.data.url)
-    setAppUrl(response.data.reclaimUrl);
+    setReclaimUrl(response.data.reclaimUrl);
   }
 
-  React.useEffect(() => {
-    if (!callbackId) return;
-
-    const interval = setInterval(() => {
-      getStatus(callbackId)
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [callbackId]);
 
   return (
     <div className="App">
       <header className="App-header">
 
-        <h2>DEMO</h2>
-        <h3>Prove that you are a real person owning a Google and Reddit account</h3>
-        <p>[This demo uses HttpsProvider and CustomProvider]</p>
+        {!reclaimUrl && 
+        <div>
+          <h2>DEMO</h2>
+          <h3>Prove that you are a real person owning a Google and Reddit account</h3>
+          <p>[This demo uses HttpsProvider and CustomProvider]</p>
 
+          <div className='actions'>
 
-
-        {!callbackUrl && <div className='actions'>
-        
-        <button
-          className="button"
-          disabled={(!!callbackUrl)}
-          onClick={proveIt}
-        >
-          Prove it!
-        </button>
+            <button
+              className="button"
+              disabled={(!!reclaimUrl)}
+              onClick={proveIt}
+            >
+              Prove it!
+            </button>
+          </div>
         </div>
-          
         }
-        {callbackUrl && 
-        <div className='links'>
-            
+        {reclaimUrl &&
+          <div className='links'>
+
             <div>If you don't have our app installed, check the installation steps {' '}
-              <a 
-                target="_blank"    
+              <a
+                target="_blank"
                 rel="noreferrer"
-                className="App-link" 
+                className="App-link"
                 href="https://questbook.gitbook.io/reclaim-protocol/installing-reclaim-wallet"
               >
-                 here
+                here
               </a>
             </div>
           </div>}
-        {status === 'verified' ? <h3>Thanks for submitting your link!</h3> : 
-        (loading ? <>
-          <div className='loader'></div>
-          {appUrl?<>
-            <h3>On mobile device?</h3>
-            <a href={appUrl} target="_blank" rel="noreferrer" className="App-link" >Click here to open on Reclaim Wallet App</a>
-            <h3>On laptop/desktop?</h3>
-            <QRCode value={appUrl} />
-            <p>or, Copy the link and send to your phone</p>
-            <input readOnly value={appUrl} />
-          </>:null} 
-        </> : <></>)}
+            {reclaimUrl ? <>
+              <h3>On mobile device?</h3>
+              <a href={reclaimUrl} target="_blank" rel="noreferrer" className="App-link" >Click here to open on Reclaim Wallet App</a>
+              <h3>On laptop/desktop?</h3>
+              <QRCode value={reclaimUrl} />
+              <p>or, Copy the link and send to your phone</p>
+              <CopyToClipboard text={reclaimUrl}>
+              <button onClick={notify}  className="button">Copy</button>
+              </CopyToClipboard>
+            </> : null}
       </header>
     </div>
   );
