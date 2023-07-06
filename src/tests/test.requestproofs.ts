c0356042e1c237c04ec8fc593e547d6e4e85a2d7
Sweta Shaw <swt.shw94@gmail.com>
2023-07-06 22:20:53 +0530

    update tests for proofs with xpath

--- src/tests/test.requestproofs.ts
+++ src/tests/test.requestproofs.ts
@@ -4,30 +4,83 @@ describe('Create proof request', () => {
 	it('should correctly create proof request', () => {
 		const reclaim = new reclaimprotocol.Reclaim()
 		const request = reclaim.requestProofs(
-			{
-				title: 'Proof of Reddit Karma',
-				baseCallbackUrl: 'https://www.google.com/',
-				callbackId: '1234',
-				requestedProofs: [
-					new reclaim.HttpsProvider({
-						name: 'Reddit',
-						logoUrl: 'https://www.redditstatic.com/desktop2x/img/favicon/android-icon-192x192.png',
-						url: 'https://www.reddit.com',
-						loginUrl: 'https://www.reddit.com/login',
-						loginCookies: ['cookie1', 'cookie2'],
-						selectionRegex: '<span class=\“_2BMnTatQ5gjKGK5OWROgaG\“>{{username}}</span>.*?<span>{{karma}} karma</span>',
-					}),
-
-					new reclaim.CustomProvider({
-						provider: 'google-login',
-						payload: {}
-					}),
-				]
-			}
+			REQUEST_PROOF_WITH_XPATH
 		)
 
-		expect(request.template.name).toBe('Proof of Reddit Karma')
+		// console.log(request.reclaimUrl)
+
+		expect(request.template.name).toBe('YC')
 		expect(request.template.claims.length).toEqual(2)
-		expect(request.callbackId).toBe('1234')
+		// expect(request.callbackId).toBe('1234')
 	})
 })
+
+const reclaim = new reclaimprotocol.Reclaim()
+
+const REQUEST_PROOF_WITHOUT_XPATH = {
+	title: 'Proof of Reddit Karma',
+	baseCallbackUrl: 'https://www.google.com/',
+	callbackId: '1234',
+	requestedProofs: [
+		new reclaim.HttpsProvider({
+			name: 'Reddit',
+			logoUrl: 'https://www.redditstatic.com/desktop2x/img/favicon/android-icon-192x192.png',
+			url: 'https://www.reddit.com',
+			loginUrl: 'https://www.reddit.com/login',
+			loginCookies: ['session', 'reddit_session', 'loid', 'token_v2', 'edgebucket'],
+			responseSelection: [
+				{ responseMatch: '<span class=\“_2BMnTatQ5gjKGK5OWROgaG\“>{{username}}</span>.*?<span>{{karma}} karma</span>' }
+			],
+		}),
+
+		new reclaim.CustomProvider({
+			provider: 'google-login',
+			payload: {}
+		}),
+	]
+}
+
+const REQUEST_PROOF_WITH_XPATH = {
+	title: 'YC',
+	baseCallbackUrl: 'https://www.google.com/',
+	requestedProofs: [
+		new reclaim.HttpsProvider({
+			name: 'YC https provider',
+			logoUrl: 'https://www.redditstatic.com/desktop2x/img/favicon/android-icon-192x192.png',
+			url: 'https://bookface.ycombinator.com/home',
+			loginUrl: 'https://bookface.ycombinator.com/home',
+			loginCookies: ['_sso.key'],
+			responseSelection: [
+				{
+					jsonPath:'$.currentUser',
+					responseMatch:'\\{"id":{{YC_USER_ID}},.*?waas_admin.*?:{.*?}.*?:\\{.*?}.*?(?:full_name|first_name).*?}',
+					xPath:"//script[@id='js-react-on-rails-context']"
+				},
+				{
+					jsonPath:'$.hasBookface',
+					responseMatch:'"hasBookface":true',
+					xPath:"//script[@data-component-name='BookfaceCsrApp']"
+				}
+			]
+		}),
+		new reclaim.HttpsProvider({
+			name: 'YC https provider',
+			logoUrl: 'https://www.redditstatic.com/desktop2x/img/favicon/android-icon-192x192.png',
+			url: 'https://bookface.ycombinator.com/home',
+			loginUrl: 'https://bookface.ycombinator.com/home',
+			loginCookies: ['_sso.key'],
+			responseSelection: [
+				{
+					jsonPath:'$.currentUser',
+					responseMatch:'\\{"id":{{YC_USER_ID}},.*?waas_admin.*?:{.*?}.*?:\\{.*?}.*?(?:full_name|first_name).*?}',
+					xPath:"//script[@id='js-react-on-rails-context']"
+				},
+				{
+					jsonPath:'$.hasBookface',
+					responseMatch:'"hasBookface":true',
+					xPath:"//script[@data-component-name='BookfaceCsrApp']"
+				}
+			]
+		})
+	]
+}
