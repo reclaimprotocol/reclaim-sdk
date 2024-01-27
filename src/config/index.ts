789c29d4d112324a8fb05d637daefbb8282f7d1c
Sweta Shaw <swt.shw94@gmail.com>
2024-01-27 11:58:31 +0530

    update template url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export const RECLAIM_APP_URL = 'https://share.reclaimprotocol.org/template/'
+export const RECLAIM_APP_URL = 'https://share.reclaimprotocol.org/requestedProofs/'
 export const RECLAIM_HANDSHAKE_URL = 'https://share.reclaimprotocol.org/handshake/'
 export const DEFAULT_CHAIN_ID = 420
